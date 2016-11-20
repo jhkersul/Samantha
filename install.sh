ad64c3e5e06c5f3fb8e2ff81c610374ff2d0f756
Kersul <joaohkfaria@gmail.com>
2016-11-20 18:56:31 -0200

    Improving install script

--- install.sh
+++ install.sh
@@ -4,42 +4,39 @@ echo "==================="
 echo "Installing packages"
 echo "==================="
 
-echo "Installing python3-pip..."
-sudo apt install python3-pip
-
 echo "Installing PyAudio..."
 sudo apt-get install portaudio19-dev python-all-dev python3-all-dev -y
-sudo pip install PyAudio==0.2.9
+pip install PyAudio==0.2.9
 
 echo "Installing SpeechRecognition..."
-sudo pip install SpeechRecognition
+pip install SpeechRecognition
 
 echo "Installing gTTS..."
-sudo pip install gTTS
+pip install gTTS
 
 echo "Installing ffmpeg..."
-sudo apt-get install ffmpeg libavcodec-extra
+sudo apt-get install -y ffmpeg libavcodec-extra
 
 echo "Installing pydub..."
-sudo pip install pydub
+pip install pydub
 
 echo "Installing Facebook API..."
-sudo pip install facebook-sdk
+pip install facebook-sdk
 
 echo "Installing GMusic libraries..."
-sudo apt-get install libav-tools libavcodec-extra
+sudo apt-get install -y libav-tools libavcodec-extra
 
 echo "Install Google Music API..."
-sudo pip install gmusicapi
+pip install gmusicapi
 
 echo "Installing VLC..."
-sudo apt-get install vlc
+sudo apt-get install -y vlc
 
 echo "Install VLC Python..."
-sudo pip install python-vlc
+pip install python-vlc
 
 echo "Installing Wikipedia"
-sudo pip install wikipedia
+pip install wikipedia
 
 echo "Installing "
-sudo pip3 install simplejson requests 
+pip install simplejson requests
